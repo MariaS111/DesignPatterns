a6cb788b991334b17a1adb13dce35f2b6bc3fc16
maria sviridova <mariasviridova470@gmail.com>
2025-04-08 23:07:31 +0300

    upd car

--- CreationalPatterns/CarAbstractFactory/car.py
+++ CreationalPatterns/CarAbstractFactory/car.py
@@ -1,11 +1,12 @@
 from abc import ABC, abstractmethod
 
-class Car:
+class Car(ABC):
     def __init__(self, weight, length, max_speed):
         self.weight = weight
         self.length = length
         self.max_speed = max_speed
 
+    @abstractmethod
     def drive(self):
         print(f"Driving {self.__class__.__name__} with "
               f"(Weight: {self.weight}, Length: {self.length}, "
