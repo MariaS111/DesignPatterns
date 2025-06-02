21ec0bde60704b8054f78f786035bd99c6980350
Maryia Svirydava <mariasviridova470@gmail.com>
2025-06-02 16:52:53 +0300

    fx

--- BehavioralPatterns/Observer/container.py
+++ BehavioralPatterns/Observer/container.py
@@ -1,10 +1,34 @@
 from typing import List
 from .observer import Observer
+from CreationalPatterns.CarAbstractFactory.car import Car
+from abc import abstractmethod, ABC
+
+
+class ContainerInterface(ABC):
+    @abstractmethod
+    def attach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def detach(self, observer: Observer) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self, subj: Car, event: str, details: dict) -> None:
+        pass
+
+    @abstractmethod
+    def add_car(self, car: Car) -> None:
+        pass
+
+    @abstractmethod
+    def check_changes(self) -> None:
+        pass
 
 
 class Container:
     def __init__(self):
-        self._cars = []
+        self._cars: List[Car] = []
         self._observers: List[Observer] = []
         self._watched_attrs = {}
 
@@ -14,16 +38,16 @@ class Container:
     def detach(self, observer: Observer) -> None:
         self._observers.remove(observer)
 
-    def notify(self, subj, event: str, details: dict = None):
+    def notify(self, subj: Car, event: str, details: dict = None) -> None:
         for observer in self._observers:
             observer.update(subj, event, details or {})
 
-    def add_car(self, car):
+    def add_car(self, car: Car) -> None:
         self._cars.append(car)
         self._watched_attrs[car] = car.__dict__.copy()
         self.notify(car, "car_added")
 
-    def check_changes(self):
+    def check_changes(self) -> None:
         for car in self._cars:
             old_attrs = self._watched_attrs[car]
             current_attrs = car.__dict__
